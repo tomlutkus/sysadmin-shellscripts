4823f136cb6f0b160c909bc43383904cc3f6eb49
Thomas Lutkus <lutkus@pm.me>
2026-01-13 11:24:09 +0000

    Added docs and header.sh

---
+++ docs/header.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Script:      script.sh
+# Description: A summary of what my script does
+# Author:      Thomas Lutkus
+# Date:        YYYY-MM-DD
+# Usage:       ./script.sh OPTIONS ARGUMENTS
+# Platform:    RHEL (10,9,8), Ubuntu (24.04,22.04,20.04), Debian (13,12,11)
+# Notes:       Additional information about dependencies and other things about the script
+
+set -euo pipefail

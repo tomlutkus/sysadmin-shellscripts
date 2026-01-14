29d950fbf671c8af6e55e215cc22ffa2ef0b6234
Thomas Lutkus <thomas@lutkus.net>
2026-01-14 21:27:38 +0000

    Progress on user_access.sh

--- server/users/user_access.sh
+++ server/users/user_access.sh
@@ -27,7 +27,7 @@ SUDO="false"
 DRY="false"
 HASH_DIR="${HOME}/env/secrets/hashes"
 PKEY_DIR="${HOME}/env/secrets/pubkeys"
-
+EXIT_CODE="0"
 
 # Functions
 usage() {
@@ -54,6 +54,17 @@ test_file() {
 	return 0
 }
 
+error_handling() {
+	local CMD_CODE="${1}"			# Capture the status of last command
+	local MSG="${2}"					# Message about the step which was taking place
+	if [[ "${CMD_CODE}" -ne 0 ]]
+	then
+		echo "${MSG} has failed. Skipping." >&2
+		EXIT_CODE="3"
+		return 1
+	fi
+	return 0
+}
 
 # Get Options
 while getopts snh OPTION
@@ -88,10 +99,10 @@ fi
 
 
 # Script logic
-for USER in "${@}"
+for NEW_USER in "${@}"
 do
-	HASH_FILE="${HASH_DIR}/${USER}.hash"
-	PKEY_FILE="${PKEY_DIR}/${USER}.pub"
+	HASH_FILE="${HASH_DIR}/${NEW_USER}.hash"
+	PKEY_FILE="${PKEY_DIR}/${NEW_USER}.pub"
 	
 	# Test if the password hash for the user exists
 	test_file "${HASH_FILE}" "hash" || continue
@@ -99,17 +110,60 @@ do
 	# Test if the pukey file for the user exists
 	test_file "${PKEY_FILE}" "pubkey" || continue
 
-	# In Progress
+	# Creates account if user doesn't exist already
+	if getent passwd "${NEW_USER}" &>/dev/null
+	then
+		echo "User ${NEW_USER} is found. Moving to next step."
+	else
+		TASK="Creating user ${NEW_USER}"
+		echo "${TASK}."
+		useradd -m -d /home/"${NEW_USER}" -s /bin/bash -c "Added by script ${0}" "${NEW_USER}"
+		error_handling "${?}" "${TASK}" || continue
+	fi
+	
+	# Sets a user password in accordance with the hash file
+	TASK="Setting the password for ${NEW_USER}"
+	echo "${TASK}."
+	echo "${NEW_USER}:$(cat ${HASH_FILE})" | chpasswd &>/dev/null
+  error_handling "${?}" "${TASK}"
+
+	# User receives sudo privilege if option was chosen
+	if [[ "${SUDO}" == "true" ]]
+	then
+		TASK="Adding user ${NEW_USER} to group wheel"
+		echo "${TASK}."
+		usermod -aG wheel "${NEW_USER}"
+		error_handling "${?}" "${TASK}"
+	fi
+
+	# Create the .ssh folder for the user
+	TASK="Creating the .ssh folder for the user ${NEW_USER}"
+	echo "${TASK}."
+	SSH_DIR="/home/${NEW_USER}/.ssh"
+	mkdir -p "${SSH_DIR}" && 	chmod 700 "${SSH_DIR}" 
+	error_handling "${?}" "${TASK}"
+
+	# Create the authorized_keys file
+	TASK="Creating the .ssh/authorized_keys file for the user ${NEW_USER}"
+	echo "${TASK}."
+	AUTH_FILE="/home/${NEW_USER}/.ssh/authorized_keys"
+	cp "${PKEY_FILE}" "${AUTH_FILE}" && chmod 600 "${AUTH_FILE}"
+	error_handling "${?}" "${TASK}"
+
+	# Change the ownership of .ssh to the user
+	chown -R "${NEW_USER}": "${SSH_DIR}"
+
+	echo "Finished processing user ${NEW_USER}."
+	echo
 
 done
 
 
-# In Progress
-
-
-
-
-
-
-
-
+if [[ "${EXIT_CODE}" -ne "0" ]]
+then
+	echo "The script has finished running. There were errors, please check the log." >&2
+	exit 1
+else
+	echo "The script has finished running. No errors."
+	exit 0
+fi

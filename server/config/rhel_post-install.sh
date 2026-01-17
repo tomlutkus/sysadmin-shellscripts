195ba54f51954cec22b9143193328ed351c21d2b
Thomas Lutkus <tomlutkus@users.noreply.github.com>
2026-01-17 21:31:32 +0000

    Added a couple of post-install scripts

---
+++ server/config/rhel_post-install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Script: rhel_post-install.sh
+# Purpose: Configure and harden RHEL post installation
+# 
+# Copyright (C) 2026 Thomas Lutkus
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Compatibility: RHEL 9+
+# Dependencies: command1, command2
+# Requires: root / user (specify privilege level)
+#
+# Usage: ./script-name.sh [options] [arguments]
+#        ./script-name.sh --help
+#
+# Author: Thomas Lutkus
+# Date: YYYY-MM-DD
+# Version: 1.0
+
+set -euo pipefail
+
+# Script constants and variables go here
